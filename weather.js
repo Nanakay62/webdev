4456402ab13495baf1ba4b1af05bbd55b980509c
Nana Kwame Osei Ofori Dickson <dic20016@byui.edu>
2022-07-13 15:59:59 +0000

    Login and Weather UI changes
    1. Backgrounds for login and weather has been changed
    2. Login redesigned to be consistent with the main app

--- weather.js
+++ weather.js
@@ -12,7 +12,7 @@ let weather = {
             document.querySelector(".city").innerText = "Weather in " + name;
             document.querySelector(".icon").src = " http://openweathermap.org/img/wn/"+ icon + ".png";
             document.querySelector(".description").innerText = description;
-            document.querySelector(".temp").innerText = temp+ "°C" ;
+            document.querySelector(".temp").innerText = temp + " °C" ;
             document.querySelector(".humidity").innerText = "Humidity " + humidity + "%";
             document.querySelector(".wind").innerText =  "Wind Speed" + speed + "km/h";
          },
@@ -25,3 +25,40 @@ document.querySelector(".search button").addEventListener("click", function() {
     weather.search();
 
 })
+
+//adding lcoal storage function.
+
+const history = document.getElementById("historyList");
+const searcher = document.getElementById("searcher");
+const searchCard = document.getElementById("search-card");
+
+let saved = localStorage.getItem("historyList") ? JSON.parse(localStorage.getItem("historyList")):[];
+
+searchCard.addEventListener("history", (e) => {
+    e.preventDefault();
+    saved.push(searcher.value);
+    localStorage.setItem("historyList", JSON.stringify(saved));
+    listBuilder(searcher.value);
+    searcher.value = "";
+  });
+
+  const listBuilder = (text) => {
+    const histories = document.createElement("li");
+    histories.innerHTML = text;
+    historyList.appendChild(histories);
+  };
+
+  //function to display history after refreshing
+  const getHistory = JSON.parse(localStorage.getItem("historyList"));
+  getHistory.forEach((histories) => {
+  listBuilder(histories);
+});
+
+// delete history
+const deleteHistory = (btn) => {
+    let el = btn.parentNode;
+    const index = [...el.parentElement.children].indexOf(el);
+    saved.splice(index, 1);
+    localStorage.setItem("histories", JSON.stringify(saved));
+    el.remove();
+  };
